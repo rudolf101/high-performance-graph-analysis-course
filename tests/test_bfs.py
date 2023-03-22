53d0cc428a7b376f242914f0b79b436292da3945
Vladislav Kasimov <70969943+rudolf101@users.noreply.github.com>
2023-03-22 18:46:43 +0300

    Refactor tests for bfs (#2)
    * Make tests parametrized
    * Now the size of the matrix is determined not by the size of the input array, but by the maximum vertex
    * Add one more test

--- tests/test_bfs.py
+++ tests/test_bfs.py
@@ -1,19 +1,14 @@
 import pathlib
 
+import pytest
+
 from project.bfs import bfs
 from tests.utils import load_test_data_json
 
 test_bfs_path = pathlib.Path(__file__).parent / "resources" / "test_bfs.json"
 
-tests = load_test_data_json(test_bfs_path)
 
-
-# TODO: Make it parametrized
-def test_bfs0():
-    matrix, source, ans = tests[0]
-    assert bfs(matrix, source) == ans
-
-
-def test_bfs1():
-    matrix, source, ans = tests[1]
+@pytest.mark.parametrize("test", load_test_data_json(test_bfs_path))
+def test_bfs(test):
+    matrix, source, ans = test
     assert bfs(matrix, source) == ans
