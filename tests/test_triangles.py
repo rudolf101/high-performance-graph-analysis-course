64f287ecaf36bab1816d4dd14eff9adedd05805e
Vladislav Kasimov <vladislavkasimov@gmail.com>
2023-03-26 23:08:50 +0300

    Add triangles count algorithms with tests

---
+++ tests/test_triangles.py
@@ -0,0 +1,16 @@
+import pathlib
+
+import pytest
+
+from project.triangles import vertex_triangle_count
+from tests.utils import load_test_data_json
+
+test_triangles_path = (
+    pathlib.Path(__file__).parent / "resources" / "test_triangles.json"
+)
+
+
+@pytest.mark.parametrize("test", load_test_data_json(test_triangles_path))
+def test_triangles(test):
+    matrix, source, ans = test
+    return vertex_triangle_count(matrix) == ans
